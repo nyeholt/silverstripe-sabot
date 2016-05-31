42b9bebb3aed6375695841789790d76472a950f5
Marcus Nyeholt <marcus@silverstripe.com.au>
2016-05-31 22:22:44 +1000

    Fixed null pointer reference

--- code/extensions/SabotControllerExtension.php
+++ code/extensions/SabotControllerExtension.php
@@ -14,10 +14,12 @@ class SabotControllerExtension extends Extension {
         Requirements::javascript('sabot/javascript/jquery.jfontsize-1.0.js');
         Requirements::javascript('sabot/javascript/accessibility-init.js');
         
-        $config = $site->SabotConfig->getValues();
-        if (count($config)) {
-            $script = 'window.sabotConfig = ' . json_encode($config) . ';';
-            Requirements::customScript($script, 'sabotconfig');
+        if ($site->SabotConfig) {
+            $config = $site->SabotConfig->getValues();
+            if (count($config)) {
+                $script = 'window.sabotConfig = ' . json_encode($config) . ';';
+                Requirements::customScript($script, 'sabotconfig');
+            }
         }
     }
 }
